c5fe0e1d3262a603f1fc4fae4ad8dd4327c3c1c8
John Wade <cowboysdude@stny.rr.com>
2017-05-19 19:40:03 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,34 +1,34 @@
 /* Magic Mirror
-    * Module: MMM-Chart
-    *
-    * By Cowboysdude
-    * 
-    */
+ * Module: MMM-Chart
+ *
+ * By Cowboysdude
+ *
+ */
 const NodeHelper = require('node_helper');
 var billboard = require("billboard-top-100").getChart;
 
 module.exports = NodeHelper.create({
-	  
+
     start: function() {
-    	console.log("Starting The Top 100 module");
+        console.log("Starting The Top 100 module");
     },
-    
+
     getChart: function() {
-    	var self = this;
-    	billboard(self.config.chart,  function(songs, err){
-      if (err) console.log(err);
-         var result = songs;
-    //console.log(songs);
-		 	self.sendSocketNotification("CHART_RESULT", result);
-       });
+        var self = this;
+        billboard(self.config.chart, function(songs, err) {
+            if (err) console.log(err);
+            var result = songs;
+            //console.log(songs);
+            self.sendSocketNotification("CHART_RESULT", result);
+        });
     },
 
     //Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, payload) {
         if (notification === "CONFIG") {
             this.config = payload;
-			} else if (notification === 'GET_CHART') {
-                this.getChart(payload);
-            }
-         }
-    });
+        } else if (notification === 'GET_CHART') {
+            this.getChart(payload);
+        }
+    }
+});
