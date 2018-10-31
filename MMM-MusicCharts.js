9296527bf79e9d821b902833ff4d9e56aaff4ca2
John Wade <johnw8017@gmail.com>
2018-10-30 21:20:13 -0400

    Add files via upload

--- MMM-MusicCharts.js
+++ MMM-MusicCharts.js
@@ -1,5 +1,5 @@
 /* Magic Mirror
- * Module: MMM-Chart
+ * Module: MMM-MusicCharts
  *
  * By cowboysdude
  *
@@ -13,8 +13,8 @@ Module.register("MMM-MusicCharts", {
         initialLoadDelay: 875, // 0 seconds delay
         retryDelay: 1500,
         fadeSpeed: 7,
-        chart: 'hot-100',
-        rotateInterval: 10 * 1000
+        rotateInterval: 10 * 1000,
+		apiKey: ""
     },
 
     getStyles: function() {
@@ -26,8 +26,7 @@ Module.register("MMM-MusicCharts", {
         Log.info("Starting module: " + this.name);
         this.sendSocketNotification('CONFIG', this.config);
 
-        // Set locale.
-        this.url = "http://www.billboard.com/rss/charts/hot-100";
+        // Set locale. 
         this.today = "";
         this.chart = {};
         this.activeItem = 0;
@@ -41,8 +40,6 @@ Module.register("MMM-MusicCharts", {
         var chart = this.chart;
 
         var wrapper = document.createElement("div");
-		wrapper.classList.add("main");
-        
 
         if (!this.loaded) {
             wrapper.classList.add("container");
@@ -56,76 +53,41 @@ Module.register("MMM-MusicCharts", {
             if (this.activeItem >= keys.length) {
                 this.activeItem = 0;
             }
-            var chart = this.chart[keys[this.activeItem]];
-            
-            var rank = document.createElement("div");
-            var lastWeek = chart.position['Last Week'];
-            rank.classList.add("xlarge", "bright", "first-div");
-            if (chart.rank === lastWeek ||lastWeek === '--'){
-			rank.innerHTML = "<img src= 'modules/MMM-MusicCharts/icons/spacer.png'>"+ chart.rank;	
-			} else if(chart.rank < lastWeek){
-			rank.innerHTML = "<img src= 'modules/MMM-MusicCharts/icons/up1.png'>"+ chart.rank;	
-			} else {
-			rank.innerHTML = "<img src= 'modules/MMM-MusicCharts/icons/down1.png'>"+ chart.rank;	
-			}
-            wrapper.appendChild(rank);
-
-            var artistLogo = document.createElement("div");
-            var artistIcon = document.createElement("img");
-            if ("http://" + chart.cover !== 'http://undefined') {
-                artistIcon.classList.add("photo", "second-div");
-                artistIcon.src = "http://" + chart.cover;
-            } else {
-                artistIcon.classList.add("photo", "second-div");
-                artistIcon.src = "modules/MMM-MusicCharts/icons/note.jpg";
+            chart = this.chart[keys[this.activeItem]];
+ 
+            var img = document.createElement("img");
+            img.classList.add("photo");
+            img.src = chart.cover;
+            img.onerror = function(event_object) {
+                var img_with_error = event_object.currentTarget
+                img_with_error.src = "modules/MMM-MusicCharts/icons/note.jpg"
             }
-            artistLogo.appendChild(artistIcon);
-            wrapper.appendChild(artistLogo);
+            wrapper.appendChild(img);
 
-            var thirdDiv = document.createElement("div");
-                        
-            
             var title = document.createElement("span");
             title.classList.add("xsmall", "bright", "span");
-            if (chart.artist != undefined){
-            title.innerHTML = "TITLE: " + chart.title;
-				} else {
-			title.innerHTML = "ARTIST: " + chart.title;		
-				}	
-            thirdDiv.appendChild(title);
+            title.innerHTML = "TITLE: " + chart.track;
+            wrapper.appendChild(title);
 
-            if (chart.artist != undefined){
             var artist = document.createElement("span");
             artist.classList.add("xsmall", "bright", "span");
-			artist.innerHTML = "ARTIST: " + chart.artist;
-            thirdDiv.appendChild(artist);
-            }
-            
-            var lastrank = document.createElement("span");
-            lastrank.classList.add("xsmall", "bright", "span");
-            lastrank.innerHTML = "Last Week Rank: #" + chart.position['Last Week'];
-            thirdDiv.appendChild(lastrank);
-
-            var peak = document.createElement("span");
-            peak.classList.add("xsmall", "bright", "span");
-            peak.innerHTML = "Highest Rank: #" + chart.position['Peak Position'];
-            thirdDiv.appendChild(peak);
+            artist.innerHTML = "ARTIST: " + chart.artist;
+            wrapper.appendChild(artist);
 
             var weeksOn = document.createElement("span");
             weeksOn.classList.add("xsmall", "bright", "span");
-            weeksOn.innerHTML = "Weeks on chart: " + chart.position['Wks on Chart'];
-            thirdDiv.appendChild(weeksOn);
-            
-            wrapper.appendChild(thirdDiv);
+            weeksOn.innerHTML = "RANK: " + chart.rank;
+            wrapper.appendChild(weeksOn);
+
         }
         return wrapper;
 
     },
 
     processChart: function(data) {
-        this.today = data.Today;
+        this.today = data;
         this.chart = data;
-    console.log(this.chart);
+        console.log(this.chart);
         this.loaded = true;
     },
 
@@ -149,7 +111,7 @@ Module.register("MMM-MusicCharts", {
     },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "CHART_RESULT") {
+        if (notification === "CHART_RESULTS") {
             this.processChart(payload);
             if (this.rotateInterval == null) {
                 this.scheduleCarousel();
