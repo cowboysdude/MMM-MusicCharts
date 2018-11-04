8a4a7fdfd588759dcefc227a60c2b0c8e6d8f2f7
John Wade <johnw8017@gmail.com>
2018-11-03 21:40:39 -0400

    Add files via upload

--- MMM-MusicCharts.js
+++ MMM-MusicCharts.js
@@ -15,7 +15,7 @@ Module.register("MMM-MusicCharts", {
         fadeSpeed: 7,
         rotateInterval: 10 * 1000,
 		apiKey: ""
-    },
+    }, 
 
     getStyles: function() {
         return ["MMM-MusicCharts.css"];
@@ -32,53 +32,51 @@ Module.register("MMM-MusicCharts", {
         this.activeItem = 0;
         this.rotateInterval = null;
         this.scheduleUpdate();
-    },
-
+    }, 
 
     getDom: function() {
 
-        var chart = this.chart;
-
-        var wrapper = document.createElement("div");
-
-        if (!this.loaded) {
-            wrapper.classList.add("container");
-            wrapper.innerHTML = "Calculating your requested chart ...<img src='modules/MMM-MusicCharts/icons/eq.gif' width=28px; height=28px;>";
-            wrapper.className = "bright small";
-            return wrapper;
-        }
-
-        var keys = Object.keys(this.chart);
+        var chart = this.chart; 
+		
+		var wrapper = document.createElement("div"); 
+        
+		    var Logoimg = document.createElement("img"); 
+			Logoimg.classList.add("imgLogo");
+            Logoimg.src = 'modules/MMM-MusicCharts/icons/test2.png'; 
+            wrapper.appendChild(Logoimg);
+			
+		   var nextDiv = document.createElement("div");
+		   nextDiv.classList.add("flexcontainer");
+           wrapper.appendChild(nextDiv);		
+		
+		var keys = Object.keys(this.chart);
         if (keys.length > 0) {
             if (this.activeItem >= keys.length) {
                 this.activeItem = 0;
             }
             chart = this.chart[keys[this.activeItem]];
+
+            var weeksOn = document.createElement("div");
+            weeksOn.classList.add("columns", "fonty");
+            weeksOn.innerHTML = chart.rank;
+            nextDiv.appendChild(weeksOn);
+			wrapper.appendChild(nextDiv); 
  
-            var img = document.createElement("img");
-            img.classList.add("photo");
+            var img = document.createElement("img"); 
+			img.classList.add("columns","img");
             img.src = chart.cover;
             img.onerror = function(event_object) {
                 var img_with_error = event_object.currentTarget
                 img_with_error.src = "modules/MMM-MusicCharts/icons/note.jpg"
             }
-            wrapper.appendChild(img);
+            nextDiv.appendChild(img);
+			wrapper.appendChild(nextDiv);
 
-            var title = document.createElement("span");
-            title.classList.add("xsmall", "bright", "span");
-            title.innerHTML = "TITLE: " + chart.track;
-            wrapper.appendChild(title);
-
-            var artist = document.createElement("span");
-            artist.classList.add("xsmall", "bright", "span");
-            artist.innerHTML = "ARTIST: " + chart.artist;
-            wrapper.appendChild(artist);
-
-            var weeksOn = document.createElement("span");
-            weeksOn.classList.add("xsmall", "bright", "span");
-            weeksOn.innerHTML = "RANK: " + chart.rank;
-            wrapper.appendChild(weeksOn);
-
+            var title = document.createElement("div");
+            title.classList.add("fontLetter","columns", "bright");
+            title.innerHTML = "TITLE: " + chart.track + "<BR>ARTIST: " + chart.artist + "<BR>PLAY COUNT: " + chart.played;
+            nextDiv.appendChild(title);
+			wrapper.appendChild(nextDiv);
         }
         return wrapper;
 
--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,9 @@
  * By Cowboysdude9
  *
  */
-const NodeHelper = require('node_helper');
+const NodeHelper = require('node_helper'); 
 const request = require("request");
-
+ 
 module.exports = NodeHelper.create({
 
     start: function() {
@@ -14,36 +14,33 @@ module.exports = NodeHelper.create({
     },
 
     getChart: function() {
-        request({
-            url: "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + this.config.apiKey + "&format=json",
-            method: 'GET'
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).tracks.track;
+    request({
+        url: "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + this.config.apiKey + "&format=json",
+        method: 'GET'
+    }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var result = JSON.parse(body).tracks.track;
 
-                var charts = [];
-                for (var i = 0; i < result.length; i++) {
-                    var name = result[i].artist.name;
-                    var title = result[i].name;
-                    var image = result[i].image[3]['#text']
-                    for (var t = 1; t <= 50; t++) {
-                        var t = t;
+            var charts = [];
+            for (var i = 0; i < result.length; i++) {
+                var name = result[i].artist.name;
+                var title = result[i].name;
+                var image = result[i].image[3]['#text'];
+				var count = result[i].playcount;
 
-                        console.log(t); //this is working it gives me 1 to 50  
-                    } //if I put chart in the above loop it gives me 50 of each 50 ......not good, makes for a VERY long list :)	
-                    var chart = {
-                        artist: name,
-                        track: title,
-                        cover: image,
-                        rank: t //this just says 51 for all of them.  This is where I'm trying to number them 1 to 50
-                    };
-                    charts.push(chart);
-                    console.log(chart);
-                }
-                this.sendSocketNotification('CHART_RESULTS', charts);
+                var chart = {
+                    artist: name,
+                    track: title,
+                    cover: image,
+                    rank: i + 1,
+					played: count
+                };
+                charts.push(chart);
             }
-        });
-    },
+            this.sendSocketNotification('CHART_RESULTS', charts);
+        } 
+    });
+},
 
     //Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, payload) {
